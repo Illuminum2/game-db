8ec0789c48906189aef5f83a2e0f20cd4bcba3a2
Illuminum2 <trost.zureden08@icloud.com>
2025-03-06 13:26:32 +0100

    First basic implementation

--- app.js
+++ app.js
@@ -1,62 +1,81 @@
-// <tr>
-// <th scope="col">Name</th>
-// <th scope="col">Price</th>
-// <th scope="col">Image</th>
-// <th scope="col">Type</th>
-// </tr>
+{/* <tr>
+    <th scope="col">Title</th>
+    <th scope="col">Genre</th>
+    <th scope="col">Release</th>
+    <th scope="col">Developer</th>
+    <th scope="col">Publisher</th>
+    <th scope="col">Description</th>
+    <th scope="col">Platforms</th>
+    <th scope="col"></th>
+</tr> */}
+
+let games = [ // Let because it will be used differently later
+    {
+        id: 0,
+        title: 'BeatSaber',
+        genre: 'Rhythm',
+        release: '21.2.2019',
+        developer: 'Beat Games',
+        publisher: 'Beat Games',
+        description: 'VR rhythm game where your goal is to slash the beats as they are coming at you',
+        platforms: ['PCVR', 'Standalone VR']
+    },
+    {
+        id: 1,
+        title: 'Bonelab',
+        genre: 'Shooter',
+        release: '29.9.2022',
+        developer: 'Stress Level Zero',
+        publisher: 'Stress Level Zero',
+        description: 'VR game where you embody an outcast escaping fate',
+        platforms: ['PCVR', 'Standalone VR']
+    }
+]
 
 const tableBody = document.querySelector('tbody')
-const baseURl = 'https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store'
-
-let cart = []
-let products = []
+// const baseURl = 'https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store'
 
 
-const init = async () => {
-    const URL = baseURl + '/products.json'
-    fetch(URL)
-    .then((result) => result.json())
-    .then((result) => {
-        products = result;
-        fillTable(result)
-    })
-}
-const addToCart = (id) => {
-    if (cart[id] = products[id]) {
-        cart[id].quantity++
-    } else {
-        cart.push(products[id])
-        cart.push(1)
-    }
+// const init = async () => {
+//     const URL = baseURl + '/products.json'
+//     fetch(URL)
+//     .then((result) => result.json())
+//     .then((result) => {
+//         products = result;
+//         fillTable(result)
+//     })
+// }
 
-    alert(`Product ${id} added.`)
-    console.clear
-    console.log(cart)
-}
-
-const fillTable = (products) => {
+const fillTable = (games) => {
     // {
-    //     "name": "baked beans",
-    //     "price": 0.4,
-    //     "image": "beans.jpg",
-    //     "type": "vegetables"
+    //     id: 1,
+    //     title: 'Bonelab',
+    //     genre: 'Shooter',
+    //     release: '29.9.2022',
+    //     developer: 'Stress Level Zero',
+    //     publisher: 'Stress Level Zero',
+    //     description: 'VR game where you embody an outcast escaping fate',
+    //     platforms: ['PCVR', 'Standalone VR']
     // }
     
     let i = 0
-    products.forEach(product => {
-        const {name, price, image, type} = product
+    games.forEach(game => {
+        const {title, genre, release, developer, publisher, description, platforms} = game
 
         const tr = document.createElement('tr')
         tr.innerHTML = `
-                <th scope="col">${name}</th>
-                <th scope="col">${price}</th>
-                <th scope="col"><image src="${ baseURl + '/images/' + image}"></image></th>
-                <th scope="col">${type}</th>
-                <td><button type="button" class="btn btn-outline-primary" onclick="addToCart(${i+1})"><i class="bi bi-cart"></i></button></th>
+                <th scope="col">${title}</th>
+                <th scope="col">${genre}</th>
+                <th scope="col">${release}</th>
+                <th scope="col">${developer}</th>
+                <th scope="col">${publisher}</th>
+                <th scope="col">${description}</th>
+                <th scope="col">${platforms}</th>
+                <th scope="col"></th>
         `
         tableBody.append(tr)
         i++
     });
 }
 
-init()
+fillTable(games)
