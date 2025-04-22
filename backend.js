e70ebf87a877f1a187039d378a5a0277a1b097fe
Illuminum2 <trost.zureden08@icloud.com>
2025-04-22 20:31:46 +0200

    Add basic backend functionality

---
+++ backend.js
@@ -0,0 +1,80 @@
+const express = requre('express')
+const { default: games } = require('./frontend/data/games.js')
+const gamesService = require('./services/games.service.js')
+
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+
+// Return ALL games
+app.get('/game', (req, res) => {
+    const games = gamesService.get()
+
+    res.send(games)
+})
+
+// Return ONE game
+app.get('/game/:id', (req, res) => {
+    const id = req.params.id
+
+    const game = gamesService.getOne(id)
+
+    res.send(game)
+})
+
+// Create game
+app.post('/game', (req, res) => {
+    const game = req.body
+
+    if (game)
+    {
+        const result = gamesServie.create(game)
+
+        res.status(201).send({message: 'Game created'})
+    }
+    else {
+        res.status(422).send({message: 'Unporcessable entity'})
+    }
+})
+
+// Update game
+app.put('/game', (req, res) => {
+    // Got id and game
+    const id = req.params.id // Not use atm
+    const game = req.body
+
+    if (game) {
+        const result = gamesService.update(game)
+
+        if (result) {
+            res.status(200).send({message: 'Game updated'})
+        }
+        else {
+            res.status(422).send({message: 'Unporcessable entity'})
+        }
+    }
+    else {
+        res.status(422).send({message: 'Unporcessable entity'})
+    }
+})
+
+// Delete game
+app.delete('/game/:id', (req, res) => {
+    // Got id
+    const id = req.params.id
+    
+    if (id) {
+        const result = gamesService.remove(id)
+
+        if (result) {
+            res.status(204).send({message: 'Game removed'})
+        }
+        else {
+            res.status(422).send({message: 'Unporcessable entity'})
+        }
+    }
+    else {
+        res.status(422).send({message: 'Unporcessable entity'})
+    }
+})
---
+++ frontend/games.service.js
@@ -0,0 +1,46 @@
+import gamesData from './data/games.js'
+
+let games = gamesData
+
+// This is fake ID
+let gamesID = gamesData.length
+
+const get = () => {
+    return games
+}
+
+const getOne = (id) => {
+    const index = games.findIndex(g => g.id === id)
+    return games[index]
+}
+
+const create = (game) => {
+    game.id = ++gamesID
+    games = [game, ...games]
+}
+
+const update = (game) => {
+    const index = games.findIndex(g => g.id === game.id)
+
+    if (index >= 0) {
+        games[index] = game
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+const remove = (id) => {
+    const index = games.findIndex(g => g.id = id)
+
+    if (index >= 0) {
+        games.splice(index, 1)
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+export default {get, getOne, create, update, remove}
