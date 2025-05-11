75440dced5f95b0e0fdfaa50da39ef5f458cd6ba
Illuminum2 <trost.zureden08@icloud.com>
2025-05-11 21:00:44 +0200

    Implement createPlatformContainer

---
+++ public/views/gameView.js
@@ -0,0 +1,183 @@
+{/* 
+    <div class="game-container">
+        <div type="button" class="col card text-bg-dark game" style="width: 18rem;" data-bs-toggle="modal" data-bs-target="#exampleModal" tabindex="0">
+            <img src="https://static1.thegamerimages.com/wordpress/wp-content/uploads/2022/11/Minecraft-Promotional-Art.jpg" class="card-img-top img-fluid rounded-top align-items-center game-img" alt="Minecraft mountains">
+            <div class="card-body">
+                <div class= "logo-container">
+                    <img class="logo" src="https://cdn.prod.website-files.com/64ea57571d50b02423c4505d/64fa5f649846f59218adca46_minecraft%20logo%20png.png" alt="Minecraft logo">
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item text-bg-dark properties-container">
+                        <span class="genre-container">
+                            <!-- Genres from IGDB -->
+                            <span class="badge rounded-pill genre g-pinball" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Pinball">
+                                <i class="bi bi-circle"></i>
+                            </span>
+                            <span class="badge rounded-pill genre g-adventure" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Adventure">
+                                <i class="bi bi-compass"></i>
+                            </span>
+                            <span class="badge rounded-pill genre g-indie" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Indie">
+                                <i class="bi bi-stars"></i>
+                            </span>
+                            <span class="badge rounded-pill genre g-arcade" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Arcade">
+                                <i class="bi bi-joystick"></i>
+                            </span>
+                            <span class="badge rounded-pill genre g-visual-novel" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Visual Novel">
+                                <i class="bi bi-book"></i>
+                            </span>
+                            <span class="badge rounded-pill genre g-board" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Card & Board Game">
+                                <i class="bi bi-dice-3"></i>
+                            </span>
+                            <span class="badge rounded-pill genre g-moba" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="MOBA">
+                                <i class="bi bi-map"></i>
+                            </span>
+                            <span class="badge rounded-pill genre g-point-click" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Point-and-click">
+                                <i class="bi bi-mouse"></i>
+                            </span>
+                            <span class="badge rounded-pill genre g-fighting" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Fighting">
+                                <i class="bi bi-lightning"></i>
+                            </span>
+                            <span class="badge rounded-pill genre g-shooter" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Shooter">
+                                <i class="bi bi-crosshair"></i>
+                            </span>
+                            <span class="badge rounded-pill genre g-music" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Music">
+                                <i class="bi bi-music-note-beamed"></i>
+                            </span>
+                            <span class="badge rounded-pill genre g-platformer" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Platformer">
+                                <i class="bi bi-box"></i>
+                            </span>
+                            <span class="badge rounded-pill genre g-puzzle" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Puzzle">
+                                <i class="bi bi-puzzle"></i>
+                            </span>
+                            <span class="badge rounded-pill genre g-racing" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Racing">
+                                <i class="bi bi-speedometer"></i>
+                            </span>
+                            <span class="badge rounded-pill genre g-rts" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Real Time Strategy">
+                                <i class="bi bi-clock-history"></i>
+                            </span>
+                            <span class="badge rounded-pill genre g-rpg" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Role-playing">
+                                <i class="bi bi-person-badge"></i>
+                            </span>
+                            <span class="badge rounded-pill genre g-simulator" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Simulator">
+                                <i class="bi bi-gear"></i>
+                            </span>
+                            <span class="badge rounded-pill genre g-sport" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Sport">
+                                <i class="bi bi-dribbble"></i>
+                            </span>
+                            <span class="badge rounded-pill genre g-strategy" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Strategy">
+                                <i class="bi bi-diagram-2"></i>
+                            </span>
+                            <span class="badge rounded-pill genre g-tbs" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Turn-based strategy">
+                                <i class="bi bi-arrow-repeat"></i>
+                            </span>
+                            <span class="badge rounded-pill genre g-tactical" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Tactical">
+                                <i class="bi bi-bullseye"></i>
+                            </span>
+                            <span class="badge rounded-pill genre g-hack-slash" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Hack and slash/Beat 'em up">
+                                <i class="bi bi-droplet"></i>
+                            </span>
+                            <span class="badge rounded-pill genre g-quiz" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Quiz/Trivia">
+                                <i class="bi bi-question-circle"></i>
+                            </span>
+                        </span>
+                        <span>
+                            <time datetime="2011-11-18">18.11.2011</time>
+                        </span>
+                    </li>
+                    <li class="list-group-item text-bg-dark description">
+                        Build, explore, and survive in a blocky, open-world sandbox with limitless creativity.
+                    </li>
+                    <li class="list-group-item text-bg-dark platform-container">
+                        <span class="badge platform p-windows" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="PC (Windows)">
+                            <i class="bi bi-windows"></i>
+                        </span>
+                        <span class="badge platform p-mac" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="MacOS">
+                            <i class="bi bi-apple"></i>
+                        </span>
+                        <span class="badge platform p-linux" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="PC (Linux)">
+                            <i class="bi bi-ubuntu"></i>
+                        </span>
+                        <span class="badge platform p-switch" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Nintendo Switch">
+                            <i class="bi bi-nintendo-switch"></i>
+                        </span>
+                        <span class="badge platform p-playstation" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Play Station">
+                            <i class="bi bi-playstation"></i>
+                        </span>
+                        <span class="badge platform p-xbox" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="X-Box">
+                            <i class="bi bi-xbox"></i>
+                        </span>
+                        <span class="badge platform p-web" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Web">
+                            <i class="bi bi-browser-chrome"></i>
+                        </span>
+                        <span class="badge platform p-steamvr" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="PCVR (SteamVR)">
+                            <i class="bi bi-steam"></i>
+                        </span>
+                        <span class="badge platform p-quest" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Meta Quest">
+                            <i class="bi bi-meta"></i>
+                        </span>
+                        <span class="badge platform p-ios" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="iOS">
+                            <i class="bi bi-apple"></i>
+                        </span>
+                        <span class="badge platform p-android" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Android">
+                            <i class="bi bi-android"></i>
+                        </span>
+                    </li>
+                    <li class="list-group-item text-bg-dark">
+                        <!--list-group-flush + list-group-horizontal currently broken in Bootstrap 5.3 -->
+                        <!-- https://github.com/twbs/bootstrap/pull/39513/commits/25ce12df7edc05ae23ce45c22e9e10e5a320e64f -->
+                        <ul class="list-group list-group-horizontal">
+                            <li class="list-group-item text-bg-dark developer">Mojang Studios</li>
+                            <li class="list-group-item text-bg-dark publisher">Mojang Studios</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="glow">
+        </div>
+    </div>
+*/}
+
+const createGameElement = (game) => {
+    // {
+    //     "id": 0,
+    //     "title": "BeatSaber",
+    //     "genres": ["music", "sport", "indie"],
+    //     "release": new date("2019-02-21"),
+    //     "description": "VR rhythm game where your goal is to slash the beats as they are coming at you",
+    //     "platforms": ["steamvr", "quest"],
+    //     "developer": "Beat Games",
+    //     "publisher": "Beat Games",
+    //     "logo": "https://static.wikia.nocookie.net/logopedia/images/c/cc/Beatsaber.png/revision/latest?cb=20220430122542",
+    //     "bg": "https://mixed-news.com/en/wp-content/uploads/2023/05/Beat-Saber-Schwerter-Bloecke.jpeg"
+    // }
+
+    const {title, genres, release, description, platforms, developer, publisher, logo, bg} = game
+
+
+    
+}
+
+const createPlatformContainer = (platforms) => {
+    const platformContainer = document.createElement('li')
+    platformContainer.classList.add('list-group-item text-bg-dark platform-container')
+    
+    platforms.forEach(platform => {
+        const iconElement = document.createElement('i')
+        iconElement.classList.add(`bi bi-${platform}`)
+
+        const platformElement = document.createElement('span')
+        platformElement.classList.add(`badge platform p-${platform}`)
+        platformElement.appendChild(iconElement)
+
+        platformElement.setAttribute("data-bs-toggle", "tooltip")
+        platformElement.setAttribute("data-bs-placement", "top")
+        platformElement.setAttribute("data-bs-title", platform)
+
+        platformContainer.appendChild(platformElement)
+    });
+
+    return platformContainer
+}
+
+export default createGameElement
