3a404787f3486cce28356af5b1beebc570bde182
Illuminum2 <trost.zureden08@icloud.com>
2025-05-11 21:00:13 +0200

    Updated storage structure

--- data/games.js
+++ data/games.js
@@ -3,32 +3,38 @@ const games = [
     {
       "id": 0,
       "title": "BeatSaber",
-      "genre": "Rhythm",
-      "release": "2019-02-21",
+      "genres": ["music", "sport", "indie"],
+      "release": new date("2019-02-21"),
+      "description": "VR rhythm game where your goal is to slash the beats as they are coming at you",
+      "platforms": ["steamvr", "quest"],
       "developer": "Beat Games",
       "publisher": "Beat Games",
-      "description": "VR rhythm game where your goal is to slash the beats as they are coming at you",
-      "platforms": ["PCVR", "Standalone VR"]
+      "logo": "https://static.wikia.nocookie.net/logopedia/images/c/cc/Beatsaber.png/revision/latest?cb=20220430122542",
+      "bg": "https://mixed-news.com/en/wp-content/uploads/2023/05/Beat-Saber-Schwerter-Bloecke.jpeg"
     },
     {
       "id": 1,
-      "title": "Bonelab",
-      "genre": "Shooter",
-      "release": "2022-09-29",
-      "developer": "Stress Level Zero",
-      "publisher": "Stress Level Zero",
-      "description": "VR game where you embody an outcast escaping fate",
-      "platforms": ["PCVR", "Standalone VR"]
+      "title": "Minecraft",
+      "genres": ["simulator", "adventure", "indie"],
+      "release": new date("2016-12-19"),
+      "description": "Build, explore, and survive in a blocky, open-world sandbox with limitless creativity",
+      "platforms": ["windows", "switch", "playstation", "xbox", "ios", "android"],
+      "developer": "Mojang Studios",
+      "publisher": "Mojang Studios",
+      "logo": "https://cdn.prod.website-files.com/64ea57571d50b02423c4505d/64fa5f649846f59218adca46_minecraft%20logo%20png.png",
+      "bg": "https://static1.thegamerimages.com/wordpress/wp-content/uploads/2022/11/Minecraft-Promotional-Art.jpg"
     },
-    {
+        {
       "id": 2,
       "title": "Bonelab",
-      "genre": "Shooter",
-      "release": "2022-09-29",
+      "genres": ["shooter", "simulator", "adventure", "indie"],
+      "release": new date("2022-09-29"),
+      "description": "VR game where you embody an outcast escaping fate",
+      "platforms": ["steamvr", "quest"],
       "developer": "Stress Level Zero",
       "publisher": "Stress Level Zero",
-      "description": "VR game where you embody an outcast escaping fate",
-      "platforms": ["PCVR", "Standalone VR"]
+      "logo": "https://cdn2.steamgriddb.com/logo/702010521c85cf7460eedc602c8b1cea.png",
+      "bg-img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgI-akPTg8fpj8QWlEgj0CSlMemgGjmVNneQ&s"
     }
   ]
 
