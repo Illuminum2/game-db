f2043d0a4b98f7fb825dd8503d138a9d87a20603
Illuminum2 <trost.zureden08@icloud.com>
2025-05-11 21:00:28 +0200

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,5 +1,7 @@
+import gamesApiService from "./services/games-api.service"
+import createGameElement from "./views/gameView"
+
 const gameContainer = document.querySelector('.game-container')
-// import gamesApiService from "./services/games-api.service"
 
 // Initialize all bootstrap tooltips
 // From Bootstrap Docs
@@ -14,48 +16,14 @@ window.addEventListener('scroll', function () {
     );
 }, true);
 
-const fillTable = (games) => {
-    // {
-    //     id: 1,
-    //     title: 'Bonelab',
-    //     genre: 'Shooter',
-    //     release: '29.9.2022',
-    //     developer: 'Stress Level Zero',
-    //     publisher: 'Stress Level Zero',
-    //     description: 'VR game where you embody an outcast escaping fate',
-    //     platforms: ['PCVR', 'Standalone VR']
-    // }
-    
+const addGames = () => {
+    let games = gamesApiService.get();
     let i = 0
     games.forEach(game => {
-        const {title, genre, release, developer, publisher, description, platforms} = game
-
-        const div = document.createElement('div')
-        div.innerHTML = `
-        <div class="card text-bg-dark mb-3 game" style="max-width: 540px;">
-            <div class="row g-0">
-                <div class="col-md-4 game-img-holder">
-                    <img src="https://picsum.photos/200/300" class="img-fluid rounded-start align-items-center game-img" alt="...">
-                </div>
-                <div class="col-md-8">
-                    <div class="card-body">
-                        <h5 class="card-title">${title}</h5>
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item text-bg-dark">${genre}</li>
-                            <li class="list-group-item text-bg-dark">${release}</li>
-                            <li class="list-group-item text-bg-dark">${developer}</li>
-                            <li class="list-group-item text-bg-dark">${publisher}</li>
-                            <li class="list-group-item text-bg-dark">${description}</li>
-                            <li class="list-group-item text-bg-dark">${platforms}</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-        `
+        const gameElement = createGameElement(game)
         gameContainer.append(div)
         i++
     });
 }
 
-//fillTable(games)
+addGames()
