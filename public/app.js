39d80812dd93987b098c2e959f71c6c1d1837949
Illuminum2 <trost.zureden08@icloud.com>
2025-05-11 23:57:50 +0200

    Initialize tooltips on games load

--- public/app.js
+++ public/app.js
@@ -5,8 +5,10 @@ const gamesContainer = document.querySelector('.games-container')
 
 // Initialize all bootstrap tooltips
 // From Bootstrap Docs
+const tooltipInit = () => {
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+}
 
 // Hide tooltips on scroll
 // Modified from https://stackoverflow.com/questions/58195249/remove-bootstrap-4-tooltip-on-page-scroll-on-mobile-and-re-enable-on-tap-on-togg
@@ -24,6 +26,8 @@ const addGames = () => {
         gameContainer.append(div)
         i++
     });
+
+    tooltipInit()
 }
 
 addGames()
